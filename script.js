59d6d72f9f05e879b980ccd87c449ee7e931340e
365NICHIKAMIHIKOUKI2024 <saonoi.bnk48thfc@gmail.com>
2024-09-11 15:48:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tweetMessage() {
-    const message = "EVE 365 Concept Photo \n#EVEBNK48 #2024_365NichinoKamihikoukiTH ";
+    const message = "EVE 365 Concept Photo \n#EveBNK48 #2024_365NichinoKamihikoukiTH ";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }  
